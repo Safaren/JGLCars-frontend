00c7448556a92642190782b373f2828cbe758d63
Safaren <101393739+Safaren@users.noreply.github.com>
2025-11-15 18:29:25 +0100

    dsdsd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default async function HomePage() {
         Coches de ocasión disponibles 🚗
       </h2>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 pb-16">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-6 pb-16">
         {cars.length > 0 ? (
           cars.map((car) => <CarCard key={car.id} car={car} />)
         ) : (
