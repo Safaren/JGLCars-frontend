288c74b745b87e40354c792b5a6d45034ce33603
Safaren <sergiomasny@gmail.com>
2025-11-20 18:03:24 +0100

    fdssdfa

--- src/app/coches/[id]/page.tsx
+++ src/app/coches/[id]/page.tsx
@@ -1,7 +1,9 @@
 
 // JGLCars-frontend/src/app/coches/[id]/page.tsx
 
-import CarPageClient from "./CarPageClient";
+import CarPageClient from "@/components/CarPageClient";
+
+
 
 export default function CarPage({ params }: { params: { id: string } }) {
   const { id } = params;
--- src/components/CarPageClient.tsx
+++ src/components/CarPageClient.tsx
@@ -30,7 +30,7 @@ export default function CarPageClient({ id }: { id: string }) {
 
     const load = async () => {
       try {
-        const res = await fetch(`/api/cars/${id}`);
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/cars/${id}`);
 
         if (!res.ok) {
           setErr("No encontrado");
