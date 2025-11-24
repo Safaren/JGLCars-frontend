b36b769d5318c4c3b3c9dd1a53b0a1cfd9eed696
Safaren <sergiomasny@gmail.com>
2025-11-24 18:36:52 +0100

    Solucion finadaeaasddasdddsdfsz<dl

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -18,6 +18,7 @@ export default function LoginPage() {
       const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
+        credentials: "include",   
         body: JSON.stringify({ email, password }),
       });
 
